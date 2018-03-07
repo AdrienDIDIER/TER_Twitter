2786227df71fb9bc0c8724f3f04a208f6c522a64
mourad <mourad.inan@hotmail.fr>
2018-03-07 23:51:08 +0100

    Modification visuel du nuage de mots.

--- static/d3/js/script-d3.js
+++ static/d3/js/script-d3.js
@@ -1,18 +1,18 @@
 var fill = d3.scale.category20();
-d3.layout.cloud().size([800, 300])
+d3.layout.cloud().size([1000, 1000])
     .words(words)
     .rotate(function() { return ~~(Math.random() * 2) * 9; })
-    .fontSize(function(d) { return d.size * 5; })
+    .fontSize(function(d) { return d.size + 8; })
     .padding(5)
     .on("end", draw)
     .start();
 function draw(words) {
     d3.select("body").append("svg")
-        .attr("width", 850)
-        .attr("height", 450)
+        .attr("width", 1000)
+        .attr("height", 1000)
         .attr("class", "wordcloud")
         .append("g")
-        .attr("transform", "translate(320,200)")
+        .attr("transform", "translate(400,400)")
         .selectAll("text")
         .data(words)
         .enter().append("text")
