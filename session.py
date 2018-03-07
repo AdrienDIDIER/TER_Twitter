f87586f9aa69bff682fd09e6a0c6f5cacd410c6c
mourad <mourad.inan@hotmail.fr>
2018-03-07 23:54:13 +0100

    Ajout du résultat de filtrage de session (nuage de mots).

--- session.py
+++ session.py
@@ -2,7 +2,8 @@ from flask import Flask,jsonify, render_template, url_for, request, session, red
 from myapp import app, mongo
 import datetime
 from auth import getUser, isLogged
-
+from retrieve_tweets.filters import filter
+from index import wordcloud
 
 @app.route('/session/add/', methods=['POST', 'GET'])
 @app.route('/session/add/<mode>', methods=['POST', 'GET'])
@@ -14,5 +15,18 @@ def addSession(mode=None):
             session_collection = mongo.db.sessions
             user_logged = getUser()
             dateOfDay = datetime.datetime.now()
-            session_collection.insert({'user_id': user_logged['_id'], 'session_name': request.form['session_name'], 'start_date' : dateOfDay.strftime("%d-%m-%y-%H-%M-%S")})
-            return "<h1>Session created</h1>"
+            documentInserted = session_collection.insert({'user_id': user_logged['_id'], 'session_name': request.form['session_name'],
+                                       'start_date': dateOfDay.strftime("%d-%m-%y-%H-%M-%S")})
+
+            session['last_session'] = str(getSessionById(documentInserted)['_id'])
+            if request.form['mode'] == 'stream':
+                filter(request.form['keywords'], None, True)
+            elif request.form['mode'] == 'dated_tweets':
+                    filter(request.form['keywords'],
+                        user=request.form['twitter_user'],
+                        startdate=request.form['start_date'],
+                        stopdate=request.form['stop_date'])
+            return wordcloud()
+
+def getSessionById(id):
+    return mongo.db.sessions.find_one(id)
