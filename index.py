923eb1e5af8a9b88ce180bcc140a92a6c46729b7
verniersolal <verniersolal@gmail.com>
2018-04-15 12:20:19 +0200

    Affiche les sessions triées par dernière date de modification, puis par dernière date de création

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, request, session, redirect
 from auth import isLogged
 from myapp import app, mongo
+from flask.ext.pymongo import PyMongo, pymongo
 
 @app.route('/')
 def index():
@@ -11,7 +12,7 @@ def index():
     if userLogged:
         users = mongo.db.users
         user = users.find_one({'email': session['email']})
-        user_sessions = mongo.db.sessions.find({'user_id': user['_id']})
+        user_sessions = mongo.db.sessions.find({'user_id': user['_id']}).sort([("last_modification_date", pymongo.DESCENDING), ("start_date", pymongo.DESCENDING)])
         sessionAmount = user_sessions.count()
     return render_template('index.html', userLogged=userLogged, user=user, user_sessions=user_sessions,
                            sessionAmount=sessionAmount)
