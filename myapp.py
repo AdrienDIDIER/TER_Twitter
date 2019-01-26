deef76aebd3fc40cf830c80fd0aa226414e8072e
mourad <mourad.inan@hotmail.fr>
2019-01-27 00:12:33 +0100

    Correction requirements.txt et début fix bugs

--- myapp.py
+++ myapp.py
@@ -4,6 +4,7 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 
 app.config.from_pyfile('database.py')
+app.config['MONGO_URI'] = 'mongodb://tweetostats:tweetostats34@ds125288.mlab.com:25288/tweetostats_db'
 
 mongo = PyMongo(app)
 
