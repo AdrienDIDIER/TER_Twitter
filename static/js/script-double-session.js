e931edafebac9167a9a47e7f27650b21acee5705
nabil-bouillin <33498018+nabil-bouillin@users.noreply.github.com>
2019-04-24 15:03:25 +0200

    Merge branch 'master' of C:\Users\default.DESKTOP-JG4MACK\PycharmProjects\TER_Twitter with conflicts.

--- static/js/script-double-session.js
+++ static/js/script-double-session.js
@@ -3,42 +3,33 @@ $(document).ready(function () {
         url: '/load_double_sessions/' + $('#session_target').attr('data-session'),
         data: '',
         type: 'GET',
-        success: function (response) {
-                setTimeout(function(){
+        success: function (response) {/**/},
+        error: function (error) {/**/}
+    });
+
+    setTimeout(function(){
         refresh_geo(1);
         refresh_tweet_polarity(false,1);
         refresh_tweet_frequency_words(false, 1);
         refresh_sunburst(false,1);
-        session2_launch();
-    }, 1200);
-        },
-        error: function (error) {/**/}
-    });
-
-
-
-function session2_launch(){
+    }, 1000);
     setTimeout(function () {
         $.ajax({
             url: '/load_double_sessions/' + $('#session_target2').attr('data-session'),
             data: '',
             type: 'GET',
-            success: function (response) {
-                console.log("script-double-session2");
-            },
-            error: function (error) {/**/
-            }
+            success: function (response) {console.log("script-double-session2");},
+            error: function (error) {/**/}
         });
-    }, 1500);
+    },10000);
 
-    setTimeout(function () {
+    setTimeout(function(){
         refresh_geo(2);
-        refresh_tweet_polarity(false, 2);
+        refresh_tweet_polarity(false,2);
         console.log("script-double-session1");
-        refresh_tweet_frequency_words(false, 2);
-        refresh_sunburst(false, 2);
-    }, 2000);
-}
+        refresh_tweet_frequency_words(false,2);
+        refresh_sunburst(false,2);
+    }, 12000);
 
 });
 
